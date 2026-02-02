151da934120fcda7f0d1af5266c425644074bc31
jihoon <leejh080628@naver.com>
2026-02-02 17:02:02 +0900

    Fix :: Decoding Key

--- Sources/PiCKAdmin/Network/APIClient.swift
+++ Sources/PiCKAdmin/Network/APIClient.swift
@@ -302,11 +302,6 @@ public struct SigninRequest: Codable {
 public struct SigninResponse: Codable {
     public let accessToken: String
     public let refreshToken: String
-
-    enum CodingKeys: String, CodingKey {
-        case accessToken = "access_token"
-        case refreshToken = "refresh_token"
-    }
 }
 
 public struct SignupRequest: Codable {
